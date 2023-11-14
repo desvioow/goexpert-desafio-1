27b6b07fcc75400b568e84b7bc06d01e00b00d48
desvioow <deliberato.lucas@gmail.com>
2023-11-13 23:39:38 -0300

    feat: persists data in sqlite + context

--- server/server.go
+++ server/server.go
@@ -1,12 +1,16 @@
-package server
+package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type UsdBrlJson struct {
@@ -15,39 +19,96 @@ type UsdBrlJson struct {
 	} `json:"USDBRL"`
 }
 
-func server() {
+func main() {
 	http.HandleFunc("/cotacao", cotacaoHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func cotacaoHandler(w http.ResponseWriter, r *http.Request) {
-	cotacaoRequest(w, r)
+
+	apiJson, err := cotacaoRequest(w, r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := retrieveDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	bid, err := strconv.ParseFloat(apiJson.USDBRL.Bid, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = insertBidIntoDb(db, bid)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
-func cotacaoRequest(w http.ResponseWriter, r *http.Request) {
+func cotacaoRequest(w http.ResponseWriter, r *http.Request) (*UsdBrlJson, error) {
 
 	ctx, cancel := context.WithTimeout(r.Context(), time.Millisecond*200)
 	defer cancel()
 	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	var apiJson UsdBrlJson
 	err = json.Unmarshal(body, &apiJson)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	json.NewEncoder(w).Encode(apiJson)
+	return &apiJson, nil
+}
+
+func retrieveDb() (*sql.DB, error) {
+
+	db, err := sql.Open("sqlite3", "sqlite.db")
+	if err != nil {
+		return nil, err
+	}
+	create_query := `CREATE TABLE IF NOT EXISTS usdxbrl (id INTEGER PRIMARY KEY AUTOINCREMENT, bid REAL);`
+
+	_, err = db.Exec(create_query)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func insertBidIntoDb(db *sql.DB, bid float64) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*10)
+	defer cancel()
+	insert_query := `INSERT INTO usdxbrl (bid) values(?) `
+
+	stmt, err := db.Prepare(insert_query)
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.ExecContext(ctx, bid)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
